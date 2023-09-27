b28afdb88354baeaba0d1455c9713999c8599e34
ashwin-godura <agodura@andrew.cmu.edu>
2023-09-27 11:35:50 -0400

    adding files form

--- superfret/app/models.py
+++ superfret/app/models.py
@@ -2,4 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class MidiFile(models.Model):
+    name = models.CharField(max_length=255)  # To store the file name
+    file = models.FileField(upload_to='midi_files/')  # To store the file itself
+    class Meta:
+        app_label = 'app'
 
+class StaticMidiFile(models.Model):
+    name = models.CharField(max_length=255)  # To store the file name
+    file = models.FileField(upload_to='midi_files/')  # To store the file itself
+    class Meta:
+        app_label = 'app'
--- superfret/app/views.py
+++ superfret/app/views.py
@@ -8,12 +8,48 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from app.forms import AddDynamicFileForm, AddStaticFileForm
+from app.models import MidiFile, StaticMidiFile
 
-def getHome(request):
-     
+def getHome(request):  
      context = {}
 
+     staticMidiFiles = StaticMidiFile.objects.all()
+     midiFiles = MidiFile.objects.all()
+
      if request.method == 'GET':
-        context['form'] = ''
-        context['output'] = ''
+        context['StaticMidiFileForm'] = AddStaticFileForm()
+        context['MidiFileForm'] = AddDynamicFileForm()
+        context['MidiFiles'] = midiFiles
+        context['StaticMidiFiles'] = staticMidiFiles
         return render(request, 'home.html', context)
+
+def addStatic(request):  
+   if request.method == 'GET':
+      return redirect(reverse('home'))
+     
+   form = StaticMidiFileForm(request.POST)
+
+   if not form.is_valid():
+       return redirect(reverse('home'))
+
+   form.save()
+   return redirect(reverse('home'))
+
+def addStatic(request):  
+   if request.method == 'GET':
+      return redirect(reverse('home'))
+     
+   form = MidiFileForm(request.POST)
+
+   if not form.is_valid():
+       return redirect(reverse('home'))
+
+   form.save()
+   return redirect(reverse('home'))
+
+
+   
+
+
+         
