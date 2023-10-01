5b83c76e2d128c1bdb9c821c0237257513eeca95
Tushaar <tushaar.jain.duke@gmail.com>
2023-10-01 11:44:32 -0400

    LEDs work

--- Capstone.ino
+++ Capstone.ino
@@ -17,6 +17,9 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 #define DELAYVAL 50  // Time (in milliseconds) to pause between pixels
 
+
+double uS_per_tick;
+
 void setup() {
   pixels.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
   Serial.begin(9600);
@@ -24,8 +27,63 @@ void setup() {
 
   parseMIDI();
 
+  uS_per_tick = (double)MICROSECONDS_PER_BEAT / (double)TICKS_PER_QUARTER_NOTE;
+  Serial.print("uS_per_tick: ");
+  Serial.println(uS_per_tick, 10);
+  printMIDI();
+}
+
+void loop() {
+  pixels.clear();  // Set all pixel colors to 'off'
+
+  for (int i = 0; i < NUM_NOTES_FOUND; i++) {
+    auto us_duration = uS_per_tick * notes[i].duration;
+    delayMicroseconds(us_duration);
+
+    auto LED_idx = notes[i].note % NUMPIXELS;
+    if (notes[i].ON) {
+      Serial.print("Waiting ");
+      Serial.print(us_duration);
+      Serial.print(" uS before turning LED ");
+      Serial.print(LED_idx);
+      Serial.print(" ON (note ");
+      Serial.print(notes[i].note);
+      Serial.println(")");
+
+      pixels.setPixelColor(notes[i].note % NUMPIXELS, pixels.Color(0, 255, 0));
+    } else {
+      Serial.print("Waiting ");
+      Serial.print(us_duration);
+      Serial.print(" uS before turning LED ");
+      Serial.print(LED_idx);
+      Serial.print(" OFF (note ");
+      Serial.print(notes[i].note);
+      Serial.println(")");
+
+      pixels.setPixelColor(notes[i].note % NUMPIXELS, pixels.Color(0, 0, 0));
+    }
+    pixels.show();  // Send the updated pixel colors to the hardware.
+  }
+
+  // for (int i = 0; i < NUMPIXELS; i++) {  // For each pixel...
+  //   pixels.setPixelColor(i - 3, pixels.Color(0, 0, 0));
+  //   pixels.setPixelColor(i - 2, pixels.Color(5, 0, 0));
+  //   pixels.setPixelColor(i - 1, pixels.Color(0, 5, 0));
+  //   pixels.setPixelColor(i, pixels.Color(0, 0, 5));
+
+  //   pixels.show();  // Send the updated pixel colors to the hardware.
+
+  //   delay(DELAYVAL);  // Pause before next pass through loop
+  // }
+}
 
 
+
+/*
+
+
+
+void printMIDI() {
   Serial.print("ticks/beat: ");
   Serial.println(TICKS_PER_QUARTER_NOTE);
 
@@ -54,21 +112,5 @@ void setup() {
 
     Serial.println("]");
   }
-
-  extern NOTE_t notes[MAX_NUM_NOTES];  // TODO hold more notes?
 }
-
-void loop() {
-  pixels.clear();  // Set all pixel colors to 'off'
-
-  for (int i = 0; i < NUMPIXELS; i++) {  // For each pixel...
-    pixels.setPixelColor(i - 3, pixels.Color(0, 0, 0));
-    pixels.setPixelColor(i - 2, pixels.Color(5, 0, 0));
-    pixels.setPixelColor(i - 1, pixels.Color(0, 5, 0));
-    pixels.setPixelColor(i, pixels.Color(0, 0, 5));
-
-    pixels.show();  // Send the updated pixel colors to the hardware.
-
-    delay(DELAYVAL);  // Pause before next pass through loop
-  }
-}
+*/
--- MIDI.h
+++ MIDI.h
@@ -22,7 +22,7 @@ uint8_t MIDI[488] = {
 };
 
 typedef struct {
-  uint32_t duration;
+  uint32_t duration; // [ticks / quarter-note]
   uint8_t note;
   bool ON;
 } NOTE_t;
@@ -197,5 +197,38 @@ void parseMIDI() {
     }
     //should not reach here
     assert(false);
+  }
+}
+
+
+
+void printMIDI() {
+  Serial.print("ticks/beat: ");
+  Serial.println(TICKS_PER_QUARTER_NOTE);
+
+  Serial.print("uS/beat: ");
+  Serial.println(MICROSECONDS_PER_BEAT);
+
+  Serial.print("NUM_NOTES_FOUND: ");
+  Serial.println(NUM_NOTES_FOUND);
+
+  for (int i = 0; i < NUM_NOTES_FOUND; i++) {
+    Serial.print("Note");
+    Serial.print(i);
+    Serial.print(": [");
+
+    Serial.print("Duration: ");
+    Serial.print(notes[i].duration);
+
+    if (notes[i].ON) {
+      Serial.print("\t ON ");
+    } else {
+      Serial.print("\t OFF ");
+    }
+
+    Serial.print(" \t Note: ");
+    Serial.print(notes[i].note);
+
+    Serial.println("]");
   }
 }
