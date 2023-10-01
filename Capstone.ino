2db8a51032848179c4458dcd3c5cb8dc9edc3f96
Tushaar <tushaar.jain.duke@gmail.com>
2023-10-01 10:44:15 -0400

    inital parsing

--- Capstone.ino
+++ Capstone.ino
@@ -3,9 +3,9 @@
 #include <avr/power.h>  // Required for 16 MHz Adafruit Trinket
 #endif
 
+#include "MIDI.h"
 
-// uint8_t MIDI[422] = { 00, C0, 00, 00, 90, 3C, 32, 81, 40, 80, 3C, 00, 81, 40, 90, 3C, 32, 81, 40, 80, 3C, 00, 81, 40, 90, 43, 3, 81, 40, 80, 43, 00, 81, 40, 90, 43, 32, 81, 40, 80, 43, 00, 81, 40, 90, 45, 32, 81, 40, 80, 45, 00, 81, 40, 90, 45, 32, 81, 40, 80, 45, 00, 81, 40, 90, 43, 32, 83, 00, 80, 43, 00, 83, 00, 90, 41, 32, 81, 40, 80, 41, 00, 81, 40, 90, 41, 32, 81, 40, 80, 41, 00, 81, 40, 90, 40, 32, 81, 40, 80, 40, 00, 81, 40, 90, 40, 32, 81, 40, 80, 40, 00, 81, 40, 90, 3E, 32, 81, 40, 80, 3E, 00, 81, 40, 90, 3E, 32, 81, 40, 80, 3E, 00, 81, 40, 90, 3C, 32, 83, 00, 80, 3C, 00, 83, 00, 90, 43, 32, 81, 40, 80, 43, 00, 81, 40, 90, 43, 32, 81, 40, 80, 43, 00, 81, 40, 90, 41, 32, 81, 40, 80, 41, 00, 81, 40, 90, 41, 32, 81, 40, 80, 41, 00, 81, 40, 90, 40, 32, 81, 40, 80, 40, 00, 81, 40, 90, 40, 32, 81, 40, 80, 40, 00, 81, 40, 90, 3E, 32, 83, 00, 80, 3E, 00, 83, 00, 90, 43, 32, 81, 40, 80, 43, 00, 81, 40, 90, 43, 32, 81, 40, 80, 43, 00, 81, 40, 90, 41, 32, 81, 40, 80, 41, 00, 81, 40, 90, 41, 32, 81, 40, 80, 41, 00, 81, 40, 90, 40, 32, 81, 40, 80, 40, 00, 81, 40, 90, 40, 32, 81, 40, 80, 40, 00, 81, 40, 90, 3E, 32, 83, 00, 80, 3E, 00, 83, 00, 90, 3C, 32, 81, 40, 80, 3C, 00, 81, 40, 90, 3C, 32, 81, 40, 80, 3C, 00, 81, 40, 90, 43, 32, 81, 40, 80, 43, 00, 81, 40, 90, 43, 32, 81, 40, 80, 43, 00, 81, 40, 90, 45, 32, 81, 40, 80, 45, 00, 81, 40, 90, 45, 32, 81, 40, 80, 45, 00, 81, 40, 90, 43, 32, 83, 00, 80, 43, 00, 83, 00, 90, 41, 32, 81, 40, 80, 41, 00, 81, 40, 90, 41, 32, 81, 40, 80, 41, 00, 81, 40, 90, 40, 32, 81, 40, 80, 40, 00, 81, 40, 90, 40, 32, 81, 40, 80, 40, 00, 81, 40, 90, 3E, 32, 81, 40, 80, 3E, 00, 81, 40, 90, 3E, 32, 81, 40, 80, 3E, 00, 81, 40, 90, 3C, 32, 83, 00, 80, 3C, 00, 00 };
-
+// extern Musical_note_TO_LED_idx[128][2];
 
 // Which pin on the Arduino is connected to the NeoPixels?
 #define PIN 6  // On Trinket or Gemma, suggest changing this to 1
@@ -21,28 +21,54 @@ void setup() {
   pixels.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
   Serial.begin(9600);
   analogReadResolution(8);
+
+  parseMIDI();
+
+
+
+  Serial.print("ticks/beat: ");
+  Serial.println(TICKS_PER_QUARTER_NOTE);
+
+  Serial.print("uS/beat: ");
+  Serial.println(MICROSECONDS_PER_BEAT);
+
+  Serial.print("NUM_NOTES_FOUND: ");
+  Serial.println(NUM_NOTES_FOUND);
+
+  for (int i = 0; i < NUM_NOTES_FOUND; i++) {
+    Serial.print("Note");
+    Serial.print(i);
+    Serial.print(": [");
+
+    Serial.print("Duration: ");
+    Serial.print(notes[i].duration);
+
+    if (notes[i].ON) {
+      Serial.print("\t ON ");
+    } else {
+      Serial.print("\t OFF ");
+    }
+
+    Serial.print(" \t Note: ");
+    Serial.print(notes[i].note);
+
+    Serial.println("]");
+  }
+
+  extern NOTE_t notes[MAX_NUM_NOTES];  // TODO hold more notes?
 }
 
-long num_reads = 1'000'0;
 void loop() {
+  pixels.clear();  // Set all pixel colors to 'off'
 
-  auto start = micros();
-  for(int i=0; i<num_reads; i++){
-    analogRead(5);
+  for (int i = 0; i < NUMPIXELS; i++) {  // For each pixel...
+    pixels.setPixelColor(i - 3, pixels.Color(0, 0, 0));
+    pixels.setPixelColor(i - 2, pixels.Color(5, 0, 0));
+    pixels.setPixelColor(i - 1, pixels.Color(0, 5, 0));
+    pixels.setPixelColor(i, pixels.Color(0, 0, 5));
+
+    pixels.show();  // Send the updated pixel colors to the hardware.
+
+    delay(DELAYVAL);  // Pause before next pass through loop
   }
-  auto end = micros();
-  Serial.println(end-start);
-
-  // pixels.clear();  // Set all pixel colors to 'off'
-
-  // for (int i = 0; i < NUMPIXELS; i++) {  // For each pixel...
-  //   pixels.setPixelColor(i - 3, pixels.Color(0, 0, 0));
-  //   pixels.setPixelColor(i - 2, pixels.Color(5, 0, 0));
-  //   pixels.setPixelColor(i - 1, pixels.Color(0, 5, 0));
-  //   pixels.setPixelColor(i, pixels.Color(0, 0, 5));
-
-  //   pixels.show();  // Send the updated pixel colors to the hardware.
-
-  //   delay(DELAYVAL);  // Pause before next pass through loop
-  // }
 }
