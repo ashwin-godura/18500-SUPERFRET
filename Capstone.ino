ae0e03e24584eb04363757571f25c628234d16a9
Tushaar <tushaar.jain.duke@gmail.com>
2023-10-03 19:45:02 -0400

    twinkle twinlke works

--- Capstone.ino
+++ Capstone.ino
@@ -35,6 +35,8 @@ void setup() {
 
 void loop() {
   pixels.clear();  // Set all pixel colors to 'off'
+  pixels.show();   // Send the updated pixel colors to the hardware.
+  delay(5000);
 
   for (int i = 0; i < NUM_NOTES_FOUND; i++) {
     auto us_duration = uS_per_tick * notes[i].duration;
@@ -64,53 +66,4 @@ void loop() {
     }
     pixels.show();  // Send the updated pixel colors to the hardware.
   }
-
-  // for (int i = 0; i < NUMPIXELS; i++) {  // For each pixel...
-  //   pixels.setPixelColor(i - 3, pixels.Color(0, 0, 0));
-  //   pixels.setPixelColor(i - 2, pixels.Color(5, 0, 0));
-  //   pixels.setPixelColor(i - 1, pixels.Color(0, 5, 0));
-  //   pixels.setPixelColor(i, pixels.Color(0, 0, 5));
-
-  //   pixels.show();  // Send the updated pixel colors to the hardware.
-
-  //   delay(DELAYVAL);  // Pause before next pass through loop
-  // }
 }
-
-
-
-/*
-
-
-
-void printMIDI() {
-  Serial.print("ticks/beat: ");
-  Serial.println(TICKS_PER_QUARTER_NOTE);
-
-  Serial.print("uS/beat: ");
-  Serial.println(MICROSECONDS_PER_BEAT);
-
-  Serial.print("NUM_NOTES_FOUND: ");
-  Serial.println(NUM_NOTES_FOUND);
-
-  for (int i = 0; i < NUM_NOTES_FOUND; i++) {
-    Serial.print("Note");
-    Serial.print(i);
-    Serial.print(": [");
-
-    Serial.print("Duration: ");
-    Serial.print(notes[i].duration);
-
-    if (notes[i].ON) {
-      Serial.print("\t ON ");
-    } else {
-      Serial.print("\t OFF ");
-    }
-
-    Serial.print(" \t Note: ");
-    Serial.print(notes[i].note);
-
-    Serial.println("]");
-  }
-}
-*/
